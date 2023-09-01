7c579260457ad5706be57308b50c07185f9ae4a1
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-09-01 16:48:31 -0500

    Initial commit

---
+++ components/navbar-actions.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Button from "@/components/ui/button";
+
+const NavbarActions = () => {
+    return (
+        <div className="ml-auto flex ic gap-x-4">
+            <Button>
+
+            </Button>
+        </div>
+    );
+}
+export default NavbarActions;
